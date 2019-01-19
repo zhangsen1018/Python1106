45c3b6a4590f3e5c2adfc778efb603c6c5f2a939
zhangsen1018 <176292872262163.com>
2019-01-20 01:23:23 +0800

    其他模板的简单创建

--- market/apps/user/views.py
+++ market/apps/user/views.py
@@ -6,7 +6,6 @@ from django.shortcuts import render, redirect
 from django.views import View
 # Create your views here.
 
-# 定义视图类
 # 在应用里创建一个加密方法
 from market import set_password
 from user.forms import RegisterModelForm, LoginModelForm
@@ -14,9 +13,8 @@ from user.helps import check_login
 from user.models import Users
 
 
-
 # 定义一个注册视图类
-class RegisterView(View): # 注册 直接定义get 和 post
+class RegisterView(View):  # 注册 直接定义get 和 post
     template_name = 'user/reg.html'
 
     def get(self, request):
@@ -28,22 +26,22 @@ class RegisterView(View): # 注册 直接定义get 和 post
         # 接收参数
         # 渲染提交的数据
         data = request.POST
-        # 验证参数合法性 用表单来验证
+        # 验证表单参数合法性 用表单来验证
         form = RegisterModelForm(data)
         if form.is_valid():
             # 操作数据库
             cleaned_data = form.cleaned_data
-            # 创建一个用户
+            # 创建一个注册用户
             # 清洗过的数据用get得到
             user = Users()
             user.username = cleaned_data.get('username')
             user.password = set_password(cleaned_data.get('password'))
             user.save()
-
             return redirect('用户:用户登录')
         else:
-            # 错误
+            # 错误信息提示
             return render(request, self.template_name, context={'form': form})
+
 
 @check_login
 # 定义一个登录视图类
@@ -54,7 +52,6 @@ class LoginView(View):  # 登录 直接定义get 和 post
     def post(self, request):
         # 接收参数
         data = request.POST
-
         # 验证数据的合法性
         form = LoginModelForm(data)
         if form.is_valid():
@@ -65,12 +62,13 @@ class LoginView(View):  # 登录 直接定义get 和 post
             request.session['ID'] = user.pk
             request.session['username'] = user.username
             # 操作数据库
-            # 返回到登录页
+            # 返回到首页
             return redirect('用户:首页')
         else:
             # 合成响应
             # 进入到登录页面
             return render(request, 'user/login.html', {'form': form})
+
 
 def index(request):
     return render(request, 'user/index.html')
